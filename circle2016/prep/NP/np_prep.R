47332823217f8e9a1a085fec0f3336da07e6b763
mburgass <m.burgass14@imperial.ac.uk>
2016-10-19 16:07:01 +0100

    update NP

--- circle2016/prep/NP/np_prep.R
+++ circle2016/prep/NP/np_prep.R
@@ -1,2 +1,2 @@
 ## Codes
-## WAL = Walrus, PWA = Pacific Walrus, HAR = Harp Seal, NAR= Narwhal, HOO = Hooded Seal.
+## WAL = Walrus, PWA = Pacific Walrus, HAR = Harp Seal, NAR= Narwhal, HOO = Hooded Sea, SPO=Spotted Seal.
