697939d6f5906ca30cab67b0c59505323f244642
mburgass <m.burgass14@imperial.ac.uk>
2016-10-25 03:07:14 -0700

    update test spatial_prep

--- circle2016/prep/spatial/spatial_prep.R
+++ circle2016/prep/spatial/spatial_prep.R
@@ -33,7 +33,7 @@ layer_arc<- 'arctic_eezs'
 poly_arc_rgn<- readOGR(dsn= spatial_dir, layer = layer_arc, stringsAsFactors = FALSE)
 
 ##Try add buffer
-arc_buffer<- rgeos::gBuffer(poly_arc_rgn, byid = FALSE, id=NULL, width=50)
+arc_buffer<- rgeos::gBuffer(poly_arc_rgn, byid = FALSE, id=NULL, width=1000) #units = m
 
 poly_arc_ebsa<- readOGR(dsn= spatial_dir, layer = 'EBSA_0511_JC_area_sort') # read in ebsa file
 p4s_arc<- CRS('+proj=laea +lat_0=90 +lon_0=-150 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0') #create p4s of arc map
