6c8ed42b08412f3b9ba790df8c9c2ae14035ec99
mburgass <m.burgass14@imperial.ac.uk>
2017-03-27 17:07:45 +0100

    add jan mayen files - some sync error

--- circle2016/conf/functions.R
+++ circle2016/conf/functions.R
@@ -1057,55 +1057,69 @@ LE = function(scores, layers){
 }
 
 
-ICO = function(layers){
+ICO = function(layers, status_year){
 
-  layers_data = SelectLayersData(layers, layers=c('ico_spp_extinction_status', 'ico_spp_popn_trend'))
-
+  layers_data = SelectLayersData(layers, layers=c('ico_spp_iucn_status'))
+  status_year<- 2016
   rk <- layers_data %>%
-    dplyr::select(region_id = id_num, sciname = category, iucn_cat=val_chr, layer) %>%
+    select(region_id = id_num, sciname = category, iucn_cat=val_chr, year, layer) %>%
     mutate(iucn_cat = as.character(iucn_cat))
 
   # lookup for weights status
-  w.risk_category = data.frame(iucn_cat = c('LC', 'NT', 'VU', 'EN', 'CR', 'EX'),
-                               risk_score = c(1,  0.8,   0.6,  0.4,  0.2,  0)) %>%
-    mutate(iucn_cat = as.character(iucn_cat))
-
-  # lookup for population trend
-  w.popn_trend = data.frame(iucn_cat = as.character(c('decreasing', 'stable', 'increasing')),
-                            trend_score = c(-0.5, 0, 0.5)) %>%
+  #  LC <- "LOWER RISK/LEAST CONCERN (LR/LC)"
+  #  NT <- "LOWER RISK/NEAR THREATENED (LR/NT)"
+  #  T  <- "THREATENED (T)" treat as "EN"
+  #  VU <- "VULNERABLE (V)"
+  #  EN <- "ENDANGERED (E)"
+  #  LR/CD <- "LOWER RISK/CONSERVATION DEPENDENT (LR/CD)" treat as between VU and NT
+  #  CR <- "VERY RARE AND BELIEVED TO BE DECREASING IN NUMBERS"
+  #  DD <- "INSUFFICIENTLY KNOWN (K)"
+  #  DD <- "INDETERMINATE (I)"
+  #  DD <- "STATUS INADEQUATELY KNOWN-SURVEY REQUIRED OR DATA SOUGHT"
+  w.risk_category = data.frame(iucn_cat = c('LC', 'NT', 'CD', 'VU', 'EN', 'CR', 'EX', 'DD'),
+                               risk_score = c(0,  0.2,  0.3,  0.4,  0.6,  0.8,  1, NA)) %>%
+    mutate(status_score = 1-risk_score) %>%
     mutate(iucn_cat = as.character(iucn_cat))
 
   ####### status
   # STEP 1: take mean of subpopulation scores
   r.status_spp <- rk %>%
-    filter(layer == 'ico_spp_extinction_status') %>%
     left_join(w.risk_category, by = 'iucn_cat') %>%
-    group_by(region_id, sciname) %>%
-    summarize(spp_mean = mean(risk_score, na.rm=TRUE) * 100) %>%
+    group_by(region_id, sciname, year) %>%
+    summarize(spp_mean = mean(status_score, na.rm=TRUE)) %>%
     ungroup()
 
   # STEP 2: take mean of populations within regions
   r.status <- r.status_spp %>%
-    group_by(region_id) %>%
+    group_by(region_id, year) %>%
     summarize(score = mean(spp_mean, na.rm=TRUE)) %>%
-    ungroup() %>%
-    mutate(dimension = "status")
-
-  ####### trend
-  # STEP 1: take mean of subpopulation scores
-  r.trend_spp <- rk %>%
-    filter(layer == 'ico_spp_popn_trend') %>%
-    left_join(w.popn_trend ,by = 'iucn_cat') %>%
-    group_by(region_id, sciname) %>%
-    summarize(spp_mean = mean(trend_score, na.rm=TRUE)) %>%
     ungroup()
 
-  # STEP 2: take mean of populations within regions
-  r.trend <- r.trend_spp %>%
+  ####### trend
+  trend_years <- c(status_year:(status_year - 9)) # trend based on 10 years of data, due to infrequency of IUCN assessments
+  adj_trend_year <- min(trend_years)
+
+
+  r.trend <- r.status %>%
     group_by(region_id) %>%
-    summarize(score = mean(spp_mean, na.rm=TRUE)) %>%
+    do(mdl = lm(score ~ year, data=., subset=year %in% trend_years),
+       adjust_trend = .$score[.$year == adj_trend_year]) %>%
+    summarize(region_id,
+              trend = ifelse(coef(mdl)['year']==0, 0, coef(mdl)['year']/adjust_trend * 5)) %>%
     ungroup() %>%
+    mutate(trend = ifelse(trend>1, 1, trend)) %>%
+    mutate(trend = ifelse(trend<(-1), (-1), trend)) %>%
+    mutate(trend = round(trend, 4)) %>%
+    select(region_id, score = trend) %>%
     mutate(dimension = "trend")
+
+
+  ####### status
+  r.status <- r.status %>%
+    filter(year == status_year) %>%
+    mutate(score = score * 100) %>%
+    mutate(dimension = "status") %>%
+    select(region_id, score, dimension)
 
   ## reference points
   rp <- read.csv('temp/referencePoints.csv', stringsAsFactors=FALSE) %>%
@@ -1117,7 +1131,47 @@ ICO = function(layers){
   # return scores
   scores <-  rbind(r.status, r.trend) %>%
     mutate('goal'='ICO') %>%
-    dplyr::select(goal, dimension, region_id, score) %>%
+    select(goal, dimension, region_id, score) %>%
+    data.frame()
+
+  ## Gapfill Oecussi Ambeno (rgn 237) with East Timor (rgn 231) data
+  ## Oecussi Ambeno is an enclave within East Timor, so the data should be very similar
+  go <- filter(scores, region_id == 231) %>%
+    mutate(region_id = 237)
+  scores <- rbind(scores, go)
+
+  ######################################
+  ## gapfill missing regions with average scores/trends of regions that share same UN geopolitical region
+  un_regions <- georegions %>%
+    select(region_id=rgn_id, r2)
+  # ID missing regions:
+  regions <- SelectLayersData(layers, layers=c('rgn_global')) %>%
+    select(region_id = id_num)
+  regions_NA <- setdiff(regions$region_id, scores$region_id)
+  scores_NA <- data.frame(goal="ICO",
+                          dimension=rep(c("status", "trend"),
+                                        each=length(regions_NA)),
+                          region_id=regions_NA, score=NA)
+
+  scores <- scores %>%
+    rbind(scores_NA) %>%
+    mutate(region_id = as.numeric(region_id)) %>%
+    left_join(un_regions, by="region_id") %>%
+    group_by(dimension, r2) %>%
+    mutate(score_gf = mean(score, na.rm=TRUE)) %>%
+    arrange(dimension, region_id) %>%
+    data.frame()
+
+  # save gapfilling records
+  scores_gf <- scores %>%
+    mutate(gapfilled = ifelse(is.na(score) & !is.na(score_gf), "1", "0")) %>%
+    mutate(method = ifelse(is.na(score) & !is.na(score_gf), "UN geopolitical avg. (r2)", NA)) %>%
+    select(goal, dimension, region_id, gapfilled, method)
+  write.csv(scores_gf, "temp/ICO_status_trend_gf.csv", row.names=FALSE)
+
+  scores <- scores %>%
+    mutate(score2 = ifelse(is.na(score), score_gf, score)) %>%
+    select(goal, dimension, region_id, score=score2) %>%
     data.frame()
 
   return(scores)
