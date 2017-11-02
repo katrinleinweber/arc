2ebf8b616dc814c82524ef5512accc84253f76b2
mburgass <m.burgass14@imperial.ac.uk>
2017-11-02 09:23:58 -0700

    calculate mean catch for reg data

--- circle2016/prep/FIS/reg/jan_mayen_reg_watson_mike.R
+++ circle2016/prep/FIS/reg/jan_mayen_reg_watson_mike.R
@@ -142,3 +142,82 @@ cmsy_bbmsy <- plyr::ldply(cmsy_fits, function(x) {
 })
 cmsy_bbmsy$model <- "CMSY"
 write.csv(cmsy_bbmsy, "circle2016/prep/FIS/reg/catch_model_bmsy_reg/cmsy_bbmsy_reg.csv", row.names=FALSE)
+
+##############################################
+## Preparing mean catch data for ohi-arc
+## Arc OHI
+###############################################
+library(dplyr)
+library(tidyr)
+source('~/github/ohiprep/src/R/common.R')
+catch <- catch <- read.csv('circle2016/prep/FIS/reg/reg_rgns/spatial_catch_reg_new.csv') %>%
+  rename(common = Common_Name, fao_id = fao_rgn, species=Scientific_Name)
+summary(catch)
+
+#Filter out non-FAO/OHI region cells and banana
+catch <- catch %>%
+  filter(!is.na(rgn_id)) %>%
+  filter(!is.na(fao_id)) %>%
+  filter(rgn_id<10)
+
+
+catch <- catch %>%
+  dplyr::select(year, rgn_id, fao_id, stock_id, TaxonKey, tons) %>%
+  group_by(rgn_id, fao_id, TaxonKey, stock_id, year) %>%
+  summarize(catch = sum(tons)) %>%
+  ungroup()
+
+#---------------------------------------------
+# for years with no reported catch, add zero values
+# (after first reported catch)
+# --------------------------------------------
+
+## these data have no zero catch values, so this is added here:
+catch_zeros <- catch %>%
+  spread(year, catch) %>%
+  data.frame() %>%
+  gather("year", "catch", num_range("X", 1950:2014)) %>%
+  mutate(year = as.numeric(gsub("X", "", year))) %>%
+  mutate(catch = ifelse(is.na(catch), 0, catch))
+
+## this part eliminates the zero catch values prior to the first reported non-zero catch
+catch_zeros <- catch_zeros %>%
+  group_by(fao_id, TaxonKey, stock_id, rgn_id) %>%
+  arrange(year) %>%
+  mutate(cum_catch = cumsum(catch)) %>%
+  filter(cum_catch > 0) %>%
+  dplyr::select(-cum_catch) %>%
+  ungroup()
+
+#---------------------------------------------
+### Calculate mean catch for ohi regions (using data from 1980 onward)
+### These data are used to weight the RAM b/bmys values
+# --------------------------------------------
+
+mean_catch <- catch_zeros %>%
+  filter(year >= 1980) %>%
+  group_by(rgn_id, fao_id, TaxonKey, stock_id) %>%
+  mutate(mean_catch=mean(catch, na.rm=TRUE))%>%
+  filter(mean_catch != 0)  %>%      ## some stocks have no reported catch for time period
+  ungroup()
+
+#---------------------------------------------
+# Toolbox formatting and save
+# --------------------------------------------
+mean_catch_toolbox <- mean_catch %>%
+  mutate(stock_id_taxonkey = paste(stock_id, TaxonKey, sep="_")) %>%
+  dplyr::select(rgn_id, stock_id_taxonkey, year, mean_catch) %>%
+  filter(year>=2001) %>%  # filter to include only analysis years
+  data.frame()
+
+
+write.csv(mean_catch_toolbox, "circle2016/prep/FIS/reg/meancatchreg/mean_catch_reg.csv", row.names=FALSE)
+
+total_catch_FP <- mean_catch %>%
+  group_by(rgn_id, year) %>%
+  summarize(fis_catch = sum(catch)) %>%
+  dplyr::select(rgn_id, year, fis_catch) %>%
+  filter(year >= 2005) # filter to include only the relevant analysis years
+
+write.csv(total_catch_FP, "circle2016/prep/FIS/reg/FP_fis_catch_reg.csv", row.names=FALSE)
+
