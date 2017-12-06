10f10a942aad78d5cc7566c467116d084b18c94b
mburgass <m.burgass14@imperial.ac.uk>
2017-12-06 11:21:43 +0000

    reg code update

--- circle2016/reg_run/reg_saup_code.R
+++ circle2016/reg_run/reg_saup_code.R
@@ -8,7 +8,7 @@ library(tidyr)
 library(dplyr)
 data<- read.csv('reg_run/reg_saup_compare.csv')
 datafp<- data %>% gather("Data", "Score", 3:4)%>%
-  filter(Goal=="Food Provision")
+  filter(Goal=="Fisheries")
 datafp$Region <- factor(datafp$Region,levels = c("Index", "Arctic Alaska",
                                                  "Nunavut", "Canadian Beaufort", "Arctic Russia", "Svalbard",
                                                  "Arctic Norway", "Jan Mayen", "West Greenland", "East Greenland"))
@@ -21,7 +21,7 @@ dataindex$Region <- factor(dataindex$Region,levels = c("Index", "Arctic Alaska",
 ggplot(datafp, aes(x = Region, y=Score, fill=Data))+
   geom_bar(stat="identity", position="dodge")+
   scale_fill_manual("Data", values = c("SAUP" = "Light Blue", "Watson.2017" = "coral2"), labels=c("SAUP", "Watson 2017"))+
-  labs(title="Food Provision", hjust=0.5)+
+  labs(title="Fisheries", hjust=0.5)+
   scale_x_discrete(labels = wrap_format(10))+
   ylim(0,100)
 
@@ -31,3 +31,5 @@ ggplot(dataindex, aes(x = Region, y=Score, fill=Data))+
   labs(title="Arctic Ocean Health Index", hjust=0.5)+
   scale_x_discrete(labels = wrap_format(10))+
   ylim(0,100)
+
+multiplot(p1,p2, cols=2)
